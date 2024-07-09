545d71bd7a6689ec3a3e4bd3a419eec6b581c3d4
eddyzags <zagabe.ed@gmail.com>
2024-07-09 12:49:58 +0200

    relayer: add forward config entry

--- pkg/relayer/config/types.go
+++ pkg/relayer/config/types.go
@@ -24,6 +24,12 @@ type YAMLRelayMinerConfig struct {
 	Pprof                  YAMLRelayMinerPprofConfig      `yaml:"pprof"`
 	SmtStorePath           string                         `yaml:"smt_store_path"`
 	Suppliers              []YAMLRelayMinerSupplierConfig `yaml:"suppliers"`
+	Foward                 []YAMLRelayMinerFowardConfig   `yaml:"forward"`
+}
+
+type YAMLRelayMinerFowardConfig struct {
+	Enabled  bool   `yaml:"enabled"`
+	JWTToken string `yaml:"jwt-token"`
 }
 
 // YAMLRelayMinerPocketNodeConfig is the structure used to unmarshal the pocket
@@ -83,6 +89,12 @@ type RelayMinerConfig struct {
 	Pprof                  *RelayMinerPprofConfig
 	Servers                map[string]*RelayMinerServerConfig
 	SmtStorePath           string
+	Forward                *RelayMinerForwardConfig
+}
+
+type RelayMinerForwardConfig struct {
+	Enabled  bool
+	JWTToken string
 }
 
 // RelayMinerPocketNodeConfig is the structure resulting from parsing the pocket
