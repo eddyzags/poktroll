ee95d31946ede939f6bf02648264df0b0870a67a
eddyzags <zagabe.ed@gmail.com>
2024-09-29 15:12:48 +0200

    relayer: add synchronous rpc forward unit tests

--- pkg/relayer/relayminer.go
+++ pkg/relayer/relayminer.go
@@ -176,6 +176,8 @@ func (rel *relayMiner) newPinghandlerFn(ctx context.Context, ln net.Listener) ht
 	})
 }
 
+// ServeForward exposes a forward HTTP server for administrators to send request to
+// specific service.
 func (rel *relayMiner) ServeForward(ctx context.Context, ln net.Listener, token string) error {
 	muxRouter := chi.NewRouter()
 	muxRouter.Method("POST", "/{service_id}", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
