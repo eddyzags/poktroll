8b1e4e1643a5ea02427d3527e7022cbc20c7528d
eddyzags <zagabe.ed@gmail.com>
2025-01-20 13:18:25 +0100

    stabilize flaky helpers in testproxy

--- pkg/relayer/proxy/proxy_test.go
+++ pkg/relayer/proxy/proxy_test.go
@@ -577,7 +577,6 @@ func (t *RelayProxyPingAllSuite) SetupSuite() {
 	appPrivateKey := secp256k1.GenPrivKey()
 	t.defaultRelayMinerServerAddress = "127.0.0.1:8245"
 	t.defaultServiceName = "defaultService"
-
 	t.supplierEndpoints = map[string][]*sharedtypes.SupplierEndpoint{
 		t.defaultServiceName: {
 			{
@@ -641,7 +640,9 @@ func (t *RelayProxyPingAllSuite) TestOKPingAllWithSingleRelayServer() {
 
 	}()
 
-	time.Sleep(time.Millisecond)
+	// waiting for relayer proxy to start
+	// and perform ping request.
+	time.Sleep(100 * time.Millisecond)
 
 	err = rp.PingAll(ctx)
 	require.NoError(t.T(), err)
@@ -716,7 +717,9 @@ func (t *RelayProxyPingAllSuite) TestOKPingAllWithMultipleRelayServers() {
 		}
 	}()
 
-	time.Sleep(time.Millisecond)
+	// waiting for relayer proxy to start
+	// and perform ping request.
+	time.Sleep(100 * time.Millisecond)
 
 	err = rp.PingAll(ctx)
 	require.NoError(t.T(), err)
@@ -804,7 +807,7 @@ func (t *RelayProxyPingAllSuite) TestNOKPingAllWithPartialFailureAfterStartup() 
 	supplierEndpoints := map[string][]*sharedtypes.SupplierEndpoint{
 		failingServiceName: {
 			{
-				Url:     "http://failingservice:8647",
+				Url:     "http://failingservice:8648",
 				RpcType: sharedtypes.RPCType_JSON_RPC,
 			},
 		},
@@ -821,7 +824,7 @@ func (t *RelayProxyPingAllSuite) TestNOKPingAllWithPartialFailureAfterStartup() 
 					ServiceConfig: &config.RelayMinerSupplierServiceConfig{
 						BackendUrl: &url.URL{
 							Scheme: "http",
-							Host:   "127.0.0.1:8647",
+							Host:   "127.0.0.1:8648",
 							Path:   "/",
 						},
 					},
@@ -859,7 +862,9 @@ func (t *RelayProxyPingAllSuite) TestNOKPingAllWithPartialFailureAfterStartup() 
 		}
 	}()
 
-	time.Sleep(time.Millisecond)
+	// waiting for relayer proxy to start
+	// and perform ping request.
+	time.Sleep(100 * time.Millisecond)
 
 	err = test.ShutdownServiceID(failingServiceName)
 	require.NoError(t.T(), err)
@@ -896,13 +901,13 @@ func (t *RelayProxyPingAllSuite) TestOKPingAllDifferentEndpoint() {
 	supplierEndpoints := map[string][]*sharedtypes.SupplierEndpoint{
 		relayminerZoneServiceName: {
 			{
-				Url:     "http://exampleservice.org:8648",
+				Url:     "http://exampleservice.org:8649",
 				RpcType: sharedtypes.RPCType_JSON_RPC,
 			},
 		},
 		relayminerIPV6ServiceName: {
 			{
-				Url:     "http://ipv6service:8649",
+				Url:     "http://ipv6service:8650",
 				RpcType: sharedtypes.RPCType_JSON_RPC,
 			},
 		},
@@ -919,7 +924,7 @@ func (t *RelayProxyPingAllSuite) TestOKPingAllDifferentEndpoint() {
 					ServiceConfig: &config.RelayMinerSupplierServiceConfig{
 						BackendUrl: &url.URL{
 							Scheme: "http",
-							Host:   "exampleservice.org:8648",
+							Host:   "exampleservice.org:8649",
 							Path:   "/",
 						},
 					},
@@ -939,7 +944,7 @@ func (t *RelayProxyPingAllSuite) TestOKPingAllDifferentEndpoint() {
 					ServiceConfig: &config.RelayMinerSupplierServiceConfig{
 						BackendUrl: &url.URL{
 							Scheme: "http",
-							Host:   "[::1]:8649",
+							Host:   "[::1]:8650",
 							Path:   "/",
 						},
 					},
@@ -977,7 +982,9 @@ func (t *RelayProxyPingAllSuite) TestOKPingAllDifferentEndpoint() {
 		}
 	}()
 
-	time.Sleep(time.Millisecond)
+	// waiting for relayer proxy to start
+	// and perform ping request.
+	time.Sleep(100 * time.Millisecond)
 
 	err = rp.PingAll(ctx)
 	require.NoError(t.T(), err)
